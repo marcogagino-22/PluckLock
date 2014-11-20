1f1eb28675a89ab91987b46aec1e4ef0330b6c98
marcogagino-22 <marcogagino.22@gmail.com>
2014-11-20 23:09:40 +0100

    code improvements

--- gen/net/syntaxblitz/plucklock/R.java
+++ gen/net/syntaxblitz/plucklock/R.java
@@ -10,10 +10,6 @@ package net.syntaxblitz.plucklock;
 public final class R {
     public static final class attr {
     }
-    public static final class color {
-        public static final int grey_200=0x7f050001;
-        public static final int red_500=0x7f050000;
-    }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
 
@@ -22,36 +18,36 @@ public final class R {
          would include 7" and 10" devices in landscape (~960dp and ~1280dp respectively).
     
          */
-        public static final int activity_horizontal_margin=0x7f060000;
-        public static final int activity_vertical_margin=0x7f060001;
+        public static final int activity_horizontal_margin=0x7f050000;
+        public static final int activity_vertical_margin=0x7f050001;
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int device_admin_text=0x7f090003;
-        public static final int enable_device_admin=0x7f090004;
-        public static final int enable_device_admin_description=0x7f090005;
-        public static final int threshold_description=0x7f090002;
-        public static final int threshold_text=0x7f090000;
-        public static final int threshold_value=0x7f090001;
+        public static final int device_admin_text=0x7f080003;
+        public static final int enable_device_admin=0x7f080004;
+        public static final int enable_device_admin_description=0x7f080005;
+        public static final int threshold_description=0x7f080002;
+        public static final int threshold_text=0x7f080000;
+        public static final int threshold_value=0x7f080001;
     }
     public static final class layout {
         public static final int settingsactivity=0x7f030000;
     }
     public static final class string {
-        public static final int admin_settings=0x7f070002;
-        public static final int app_name=0x7f070000;
-        public static final int enable_device_admin=0x7f070004;
-        public static final int enable_device_admin_description=0x7f070005;
-        public static final int failed_to_lock=0x7f070008;
-        public static final int threshold=0x7f070001;
-        public static final int threshold_description=0x7f070003;
-        public static final int threshold_too_high=0x7f070007;
-        public static final int threshold_too_low=0x7f070006;
+        public static final int admin_settings=0x7f060002;
+        public static final int app_name=0x7f060000;
+        public static final int enable_device_admin=0x7f060004;
+        public static final int enable_device_admin_description=0x7f060005;
+        public static final int failed_to_lock=0x7f060008;
+        public static final int threshold=0x7f060001;
+        public static final int threshold_description=0x7f060003;
+        public static final int threshold_too_high=0x7f060007;
+        public static final int threshold_too_low=0x7f060006;
     }
     public static final class style {
-        public static final int AppTheme=0x7f080000;
+        public static final int AppTheme=0x7f070000;
     }
     public static final class xml {
         public static final int admin=0x7f040000;
